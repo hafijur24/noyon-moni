ef65e24070994674f860e0c0aecb30884fa49845
Hafizur Rahman <mdhafijur5865@gmail.com>
2026-04-25 09:17:49 -0700

    script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+const hour = document.getElementById('hour')
+const minute = document.getElementById('minute')
+const second = document.getElementById('second')
+const day = document.getElementById('day')
+
+
+var monthName =[
+    'January' ,
+    'February' ,
+    'March' ,
+    'April' ,
+    'May' ,
+    'June' ,
+    'July' ,
+    'Argust' ,
+    'September' ,
+    'October' ,
+    'November' ,
+    'December' ,
+
+]
+const clock=setInterval(function time(){
+    let today = new Date()
+    let d =today.getDate()
+    let m =today.getMonth()
+    let y =today.getFullYear()
+    let h =today.getHours()
+    let min =today.getMinutes()
+    let sec=today.getSeconds()
+
+    day.innerHTML = '15 April 2026 '
+    hour.textContent=h
+    minute.innerText=min
+    second.innerText=sec
+
+})
+
+
